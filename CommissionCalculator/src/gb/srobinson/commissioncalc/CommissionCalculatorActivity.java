6af2fd5f8d7e40701e86be52ce2d304cfa60caef
Game <Game@games-PC>
2011-09-08 18:37:18 +0100

    Project Start

---
+++ CommissionCalculator/src/gb/srobinson/commissioncalc/CommissionCalculatorActivity.java
@@ -0,0 +1,28 @@
+package gb.srobinson.commissioncalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class CommissionCalculatorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        ListView menulist = (ListView) findViewById(R.id.ListView_Menu) ;
+        
+        String[] items = { getResources().getString(R.string.menu_item_new),
+        		getResources().getString(R.string.menu_item_load),
+        		getResources().getString(R.string.menu_item_historical) };
+        
+        ArrayAdapter<String> adapt = new ArrayAdapter<String>(this,
+        		R.layout.menu_items, items);
+        
+        menulist.setAdapter(adapt);
+        
+        }
+        
+        
+    }

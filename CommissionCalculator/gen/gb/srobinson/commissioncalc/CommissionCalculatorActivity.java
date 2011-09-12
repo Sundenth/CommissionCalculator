4401c5bc757a2658526d6a0a1e62e3d55060bbad
Stuart Robinson <stuart.robinson1987@gmail.com>
2011-09-12 21:17:28 +0100

    addition of menu coding

--- CommissionCalculator/gen/gb/srobinson/commissioncalc/CommissionCalculatorActivity.java
+++ CommissionCalculator/gen/gb/srobinson/commissioncalc/CommissionCalculatorActivity.java
@@ -1,9 +1,17 @@
 package gb.srobinson.commissioncalc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+
+
+
 
 public class CommissionCalculatorActivity extends Activity {
     /** Called when the activity is first created. */
@@ -18,12 +26,39 @@ public class CommissionCalculatorActivity extends Activity {
         		getResources().getString(R.string.menu_item_historical),
         		getResources().getString(R.string.menu_item_graph) };
         
-        ArrayAdapter<String> adapt = new ArrayAdapter<String>(this,
-        		R.layout.menu_items, items);
+     
         
+        ArrayAdapter<String> adapt = new ArrayAdapter<String>(this, R.layout.menu_items, items);
         menulist.setAdapter(adapt);
+        menulist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position, long id) {
+                TextView textView = (TextView) itemClicked;
+                String strText = textView.getText().toString();
+                if (strText.equalsIgnoreCase(getResources().getString(R.string.menu_item_new))) {
+                    // Launch the new Activity
+                    startActivity(new Intent(CommissionCalculatorActivity.this, newActivity.class));
+                } else if (strText.equalsIgnoreCase(getResources().getString(R.string.menu_item_load))) {
+                    // Launch the load Activity
+                    startActivity(new Intent(CommissionCalculatorActivity.this, loadActivity.class));
+                } else if (strText.equalsIgnoreCase(getResources().getString(R.string.menu_item_historical))) {
+                    // Launch the historical Activity
+                    startActivity(new Intent(CommissionCalculatorActivity.this, historicalActivity.class));
+                } else if (strText.equalsIgnoreCase(getResources().getString(R.string.menu_item_graph))) {
+                    // Launch the graphs Activity
+                    startActivity(new Intent(CommissionCalculatorActivity.this, graphsActivity.class));
+                }
+
+			}
+        });
+
+    }; 
+        
+     
+        
         
         }
         
         
-    }
+    
+
